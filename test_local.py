f11785fbd52a4c9b931eb6f76bdc6e348b94314a
Chris Emerson <cemerson@den-lt-cemerson.local>
2018-05-22 13:30:22 -0600

    Fixed incorrect url for api test

--- test_local.py
+++ test_local.py
@@ -35,9 +35,8 @@ def test_http_endpoint():
     """
     Test that the BaseHttpEndpoint class can make a GET to a base url
     """
-    github_api_endpoint = BaseHttpEndpoint("https://qa.i3ballot.net/api")
-    response_body = github_api_endpoint.GET('/voter/branding').json()
-    print(response_body)
+    github_api_endpoint = BaseHttpEndpoint("https://api.github.com")
+    response_body = github_api_endpoint.GET('/').json()
     assert response_body["current_user_url"] == "https://api.github.com/user"
 
 
