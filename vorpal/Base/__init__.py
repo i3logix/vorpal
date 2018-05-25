70e24761c83c61b7b7d0b2506b45bfe928f70897
Chris Emerson <cemerson@den-lt-cemerson.local>
2018-05-25 12:19:37 -0600

    Added missing reference to WebdriverFactory and updated version number in setup.py

--- vorpal/Base/__init__.py
+++ vorpal/Base/__init__.py
@@ -1,1 +1,2 @@
 from .page import BasePage
+from .webdriver_factory import WebDriverFactory
--- vorpal/__init__.py
+++ vorpal/__init__.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from .Base import custom_selenium_driver, BasePage
+from .Base import custom_selenium_driver, BasePage, WebDriverFactory
 from .ElementWrapper import ExtendedWebElement
 from .BaseHttpEndpoint import BaseHttpEndpoint
