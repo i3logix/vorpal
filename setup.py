85aaf168c5cf11a5625db373e1dd6a60957d090e
Chris Emerson <cemerson@i3logix.com>
2018-07-10 13:59:57 -0600

    Added options for remote webdriver to webdriver_factory
    Signed-off-by: Chris Emerson <cemerson@i3logix.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vorpal',
-    version='3.1.1',
+    version='3.2.2',
     description='End-to-end testing framework',
     long_description='End-to-end testing framework',
     long_description_content_type='text/markdown',
--- vorpal/Base/webdriver_factory.py
+++ vorpal/Base/webdriver_factory.py
@@ -4,11 +4,12 @@ WebDriver Factory class implementation.
 Creates a driver instance based on various browser configurations.
 """
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from .custom_selenium_driver import CustomSeleniumDriver
 
 class WebDriverFactory:
     
-    def __init__(self, browser, base_url, webdriver = webdriver):
+    def __init__(self, browser: str, base_url: str, webdriver = webdriver, remote: bool = False, remote_url: str = 'http://127.0.0.1:4444/wd/hub'):
         """
         Initialize WebDriverFactory class.
         :param browser: specified browser.
@@ -17,21 +18,33 @@ class WebDriverFactory:
         self.browser = browser
         self.base_url = base_url
         self.webdriver = webdriver
+        self.remote = remote
+        self.remote_url = remote_url
 
-    def get_webdriver_instance(self, waiting_time=5) -> CustomSeleniumDriver:
+        self.desired_capabilities: DesiredCapabilities = {
+            'chrome': DesiredCapabilities.CHROME,
+            'firefox': DesiredCapabilities.FIREFOX,
+            'IE': DesiredCapabilities.INTERNETEXPLORER,
+        }.get(browser, DesiredCapabilities.CHROME)
+
+    def get_webdriver_instance(self, waiting_time: int = 5) -> CustomSeleniumDriver:
         """
         Get WebDriver Instance based on the browser configuration.
         :param waiting_time: Implicit wait time for all elements on a web page.
         :return: Webdriver instance.
         """
-
-        if self.browser == "firefox":
-            driver = self.webdriver.Firefox()
-        elif self.browser == "IE":
-            driver = self.webdriver.Ie()
+        if self.remote:
+            driver = self.webdriver.Remote(
+                command_executor=self.remote_url,
+                desired_capabilities=self.desired_capabilities)
         else:
-            driver = self.webdriver.Chrome()
-            driver.set_window_size(1920, 1080)
+            if self.browser == "firefox":
+                driver = self.webdriver.Firefox()
+            elif self.browser == "IE":
+                driver = self.webdriver.Ie()
+            else:
+                driver = self.webdriver.Chrome()
+                driver.set_window_size(1920, 1080)
 
         driver.implicitly_wait(waiting_time)
         driver.maximize_window()
