f9c82807a3878bb1ed7048682fe70eefe75f6da9
Chris Emerson <cemerson@i3logix.com>
2018-07-24 11:14:59 -0600

    Added expected conditions file w a wait_until function
    Signed-off-by: Chris Emerson <cemerson@i3logix.com>

---
+++ vorpal/Base/expected_condition.py
@@ -0,0 +1,10 @@
+"""Methods to handle waiting for a condition to be true"""
+from time import sleep
+from typing import Callable
+
+
+def wait_until(condition_function: Callable[[], bool], timeout_seconds: float=5, polling_frequency_seconds: float=0.5):
+    time_waited = 0
+    while not condition_function():
+        sleep(polling_frequency_seconds)
+        time_waited += polling_frequency_seconds
